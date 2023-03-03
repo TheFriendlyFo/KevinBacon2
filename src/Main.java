1370cfd6233a80e61df5c353b664f5c8c12e3f79
SethV <seth.vizel@gmail.com>
2023-03-02 22:54:53 -0500

    Fully done

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        SixDegreesToKevinBacon runner = new SixDegreesToKevinBacon();
+        runner.runTheBaconater();
+    }
+}
